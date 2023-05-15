cddd373bd40336287b249fc6b3fa439047345b57
Seppuku <Iamemperor@yandex.ru>
2023-05-15 18:59:34 +0300

    changes in branch

--- empty_file4.py
+++ empty_file4.py
@@ -1,7 +1,5 @@
 print('work')
 # Comment
-for i in range(5):
-    for j in range(1):
-        print('Hello!WORLD!')
-a = list(range(1, 11))
-print(a)
+a = 12
+b = 13
+print(a + b)
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 # comment
 for i in range(5):
     print('Hello!')
+a = 32904
+print(a)
